afdce4445dec2cefc4cf240afdb54fff8ecc4ce3
Sashong <sashong.santhosh@soexcellence.com>
2026-04-15 19:58:25 +0530

    bug fixed and deliveryactivites updated

--- src/app/Product Designer/delivery-set/formtemplate/formtemplate.component.ts
+++ src/app/Product Designer/delivery-set/formtemplate/formtemplate.component.ts
@@ -21,6 +21,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTimepickerModule } from '@angular/material/timepicker';
 
@@ -61,7 +62,8 @@ export interface FlippingQuestionValue {
     MatButtonModule,
     MatInputModule,
     MatCheckboxModule,
-    MatTimepickerModule
+    MatTimepickerModule,
+    MatTooltipModule
   ],
   providers: [
     provideNativeDateAdapter()
@@ -89,10 +91,10 @@ export class FormtemplateComponent {
   userProfile:any
   queueData:any
   participantQueueToken:any
-  // form submit 
+  // form submit
   submissionComplete: boolean = false;
   submittedFormName: string = '';
-  
+
   @Input() isInline: boolean = false;
   @Output() formSubmitted = new EventEmitter<void>();
 
@@ -115,10 +117,10 @@ export class FormtemplateComponent {
      // Get queue ID from route params
     this.queueId = this.inlineQueueId ?? this.route.snapshot.queryParams['queueid'] ?? null;
     this.formpatch = ![null,undefined].includes(this.route.snapshot.queryParams['patchdata']) ? true : (![null,undefined].includes(this.participantformtemplateid) ? true : false)
-    
+
     // Get user ID and roles in constructor
     await this.initializeUserData();
-    
+
     // If queue ID exists, fetch queue data
     if (this.queueId) {
       await this.initializeQueueData();
@@ -127,7 +129,7 @@ export class FormtemplateComponent {
 
   ngAfterViewInit(){
     // console.log(" ngAfterViewInit participantformtemplateid",this.participantformtemplateid);
-    
+
     // console.log(this.formpatch);
     // this.queueId = this.route.snapshot.queryParams['queueid'] ?? null
     this.patchformid = this.inlineFormId ?? this.route.snapshot.queryParams['id']
@@ -196,7 +198,7 @@ export class FormtemplateComponent {
             }
           }
           //form setup ended and form patch started
-          n = 0 
+          n = 0
           for (let i = 0; i < this.submittedClientForm['formarray'].length; i++) {
             const element = this.submittedClientForm['formarray'][i];
             if(!['label','video','audio'].includes(element['type'])){
@@ -234,7 +236,7 @@ export class FormtemplateComponent {
           let url = form.options[0];
           url += (url.includes('?')?'&':'?') + 'ngsw-bypass';
           form.options = [url]
-        } 
+        }
         return form
       })
       // console.log('modified : ',this.submittedClientForm)
@@ -258,7 +260,7 @@ export class FormtemplateComponent {
       // Get queue document
       const queueDocRef = doc(this.firestore, 'queue generation', this.queueId!);
       const queueDoc = await getDoc(queueDocRef);
-      
+
       if (queueDoc.exists()) {
         this.queueData = queueDoc.data();
       }
@@ -407,7 +409,7 @@ export class FormtemplateComponent {
 
    private async processFormSubmission(value: any) {
     this.deliveryForm.reset();
-    
+
     const loadingRef = this.dialog.open(LoadingProgressComponent, {
       data: { msg: "Submitting Please Wait ..." },
       disableClose: true
@@ -416,7 +418,7 @@ export class FormtemplateComponent {
     try {
       // Process form array values
       this.processFormArrayValues(value);
-      
+
       // Process array type controls
       this.processArrayTypeControls();
 
@@ -441,7 +443,7 @@ export class FormtemplateComponent {
 
       // Submit the form
       await this.submitFormData(nextstage);
-      
+
       loadingRef.close();
       if (this.queueId) {
         this.submittedFormName = this.submittedClientForm['formname'];
@@ -466,7 +468,7 @@ export class FormtemplateComponent {
     for (let i = 0; i < this.submittedClientForm.formarray.length; i++) {
       const element = this.submittedClientForm.formarray[i];
       if (!['label', 'video', 'audio'].includes(element['type'])) {
-        element["value"] = (value[`control${n}`] !== undefined && value[`control${n}`] !== null) 
+        element["value"] = (value[`control${n}`] !== undefined && value[`control${n}`] !== null)
           ? value[`control${n}`] : null;
         n++;
       }
@@ -476,18 +478,18 @@ export class FormtemplateComponent {
   private processArrayTypeControls() {
     for (let i = 0; i < this.submittedClientForm.formarray.length; i++) {
       const formelement = this.submittedClientForm.formarray[i];
-      
-      if (formelement['type'] === 'array' && 
+
+      if (formelement['type'] === 'array' &&
           ![null, undefined].includes(this.submittedClientForm.formarray[i]['value'])) {
-        
+
         // Map arraycontrol keys to field names
         for (let j = 0; j < this.submittedClientForm.formarray[i]['value'].length; j++) {
           const valueObj = this.submittedClientForm.formarray[i]['value'][j];
-          
+
           for (const key in valueObj) {
             const index = Object.keys(valueObj).indexOf(key);
             const formcontrol = `arraycontrol${index}`;
-            
+
             if (formelement['array'][index] && formelement['array'][index]['fieldname']) {
               valueObj[formelement['array'][index]['fieldname']] = valueObj[formcontrol];
             }
@@ -497,7 +499,7 @@ export class FormtemplateComponent {
         // Remove arraycontrol keys
         for (let j = 0; j < this.submittedClientForm.formarray[i]['value'].length; j++) {
           const valueObj = this.submittedClientForm.formarray[i]['value'][j];
-          
+
           for (const key in valueObj) {
             const index = Object.keys(valueObj).indexOf(key);
             const formcontrol = `arraycontrol${index}`;
@@ -512,7 +514,7 @@ export class FormtemplateComponent {
     if (this.queueId) {
       const queueDocRef = doc(this.firestore, 'queue generation', this.queueId);
       this.submittedClientForm['queueref'] = queueDocRef;
-      
+
       if (this.participantQueueToken) {
         this.submittedClientForm['queuetokenref'] = this.participantQueueToken.ref;
         this.submittedClientForm['stagename'] = this.participantQueueToken['currentstage'];
@@ -532,18 +534,18 @@ export class FormtemplateComponent {
       // Get next stage from variation
       const variationDocRef = doc(this.firestore, 'queue variation', variationId);
       const variationDoc = await getDoc(variationDocRef);
-      
+
       if (variationDoc.exists()) {
         const stages = variationDoc.data()['stages'];
         const currentIndex = stages.indexOf(currentStage);
-        return currentIndex !== -1 && currentIndex < stages.length - 1 
+        return currentIndex !== -1 && currentIndex < stages.length - 1
           ? stages[currentIndex + 1] : null;
       }
     } else if (this.queueData) {
       // Get next stage from queue data
       const stages = this.queueData['stages'];
       const currentIndex = stages.indexOf(currentStage);
-      return currentIndex !== -1 && currentIndex < stages.length - 1 
+      return currentIndex !== -1 && currentIndex < stages.length - 1
         ? stages[currentIndex + 1] : null;
     }
 
@@ -571,7 +573,7 @@ export class FormtemplateComponent {
         status: "completed"
       });
       await this.auth.updateDeliveryStatus(formDocRef.path, "completed");
-      
+
     } else if (this.participantQueueToken && nextstage) {
       // Update queue token for queue submissions
       await this.updateQueueToken(nextstage);
@@ -639,7 +641,7 @@ export class FormtemplateComponent {
           const patchDataPath = this.route.snapshot.queryParams['patchdata'];
           const existingFormDocRef = doc(this.firestore, patchDataPath);
           const existingFormDoc = await getDoc(existingFormDocRef);
-          
+
           if (existingFormDoc.exists()) {
             // Create log entry in formsByClient log collection
             const logDocRef = doc(this.firestore, 'formsByClient log', this.draftDocid);
@@ -651,7 +653,7 @@ export class FormtemplateComponent {
           for (let i = 0; i < this.submittedClientForm.formarray.length; i++) {
             const element = this.submittedClientForm.formarray[i];
             if (!['label', 'video', 'audio'].includes(element['type'])) {
-              element["value"] = (value[`control${n}`] !== undefined && value[`control${n}`] !== null) 
+              element["value"] = (value[`control${n}`] !== undefined && value[`control${n}`] !== null)
                 ? value[`control${n}`] : null;
               n++;
             }
@@ -662,18 +664,18 @@ export class FormtemplateComponent {
           // Process array type controls - same logic as before
           for (let i = 0; i < this.submittedClientForm.formarray.length; i++) {
             const formelement = this.submittedClientForm.formarray[i];
-            
-            if (formelement['type'] === 'array' && 
+
+            if (formelement['type'] === 'array' &&
                 ![null, undefined].includes(this.submittedClientForm.formarray[i]['value'])) {
-              
+
               // Map arraycontrol keys to field names
               for (let j = 0; j < this.submittedClientForm.formarray[i]['value'].length; j++) {
                 const valueObj = this.submittedClientForm.formarray[i]['value'][j];
-                
+
                 for (const key in valueObj) {
                   const index = Object.keys(valueObj).indexOf(key);
                   const formcontrol = `arraycontrol${index}`;
-                  
+
                   if (formelement['array'][index] && formelement['array'][index]['fieldname']) {
                     valueObj[formelement['array'][index]['fieldname']] = valueObj[formcontrol];
                   }
@@ -683,7 +685,7 @@ export class FormtemplateComponent {
               // Remove arraycontrol keys
               for (let j = 0; j < this.submittedClientForm.formarray[i]['value'].length; j++) {
                 const valueObj = this.submittedClientForm.formarray[i]['value'][j];
-                
+
                 for (const key in valueObj) {
                   const index = Object.keys(valueObj).indexOf(key);
                   const formcontrol = `arraycontrol${index}`;
@@ -697,11 +699,11 @@ export class FormtemplateComponent {
 
           // Set form metadata
           this.submittedClientForm['docid'] = this.draftDocid;
-          
+
           // Get user roles for editedby field
           const roles = await this.auth.getRoles();
           this.submittedClientForm["editedby"] = roles.profile_ref.id;
-          
+
           this.submittedClientForm['date'] = new Date();
           this.submittedClientForm['formid'] = this.inlineFormId ?? this.patchformid ?? null;
           this.submittedClientForm["submittedin"] = "starlabs";
@@ -710,7 +712,7 @@ export class FormtemplateComponent {
 
           // Update the document using merge option
           await setDoc(existingFormDocRef, this.submittedClientForm, { merge: true });
-          
+
           loadingRef.close();
 
         } catch (error) {
@@ -724,16 +726,16 @@ export class FormtemplateComponent {
   async autoSave(value: any) {
     console.log(value);
     console.log(this.submittedClientForm)
-    
+
     try {
       // Process form array values
       let e = 0;
       for (let i = 0; i < this.submittedClientForm.formarray.length; i++) {
         const element = this.submittedClientForm.formarray[i];
-        
+
         if (!['label', 'video', 'audio'].includes(element['type'])) {
           console.log(value[`control${e}`]);
-          element["value"] = (value[`control${e}`] !== undefined && value[`control${e}`] !== null) 
+          element["value"] = (value[`control${e}`] !== undefined && value[`control${e}`] !== null)
             ? value[`control${e}`] : null;
           e++;
         }
@@ -755,7 +757,7 @@ export class FormtemplateComponent {
       // Save to temporary_forms collection using modern Firebase syntax
       const tempFormDocRef = doc(this.firestore, 'temporary_forms', this.draftDocid);
       await setDoc(tempFormDocRef, this.submittedClientForm, { merge: true });
-      
+
       console.log("Temporary form submitted");
 
     } catch (error) {
@@ -769,11 +771,11 @@ export class FormtemplateComponent {
       return
     }
     console.log("Forms Draft");
-    
+
     try {
       const draftforms: QueryDocumentSnapshot[] = [];
       console.log(this.profileid);
-      
+
       // Query temporary_forms collection with modern Firebase syntax
       const tempFormsCollectionRef = collection(this.firestore, 'temporary_forms');
       const draftQuery = query(
@@ -781,10 +783,10 @@ export class FormtemplateComponent {
         where('formid', '==', this.patchformid),
         where('profileid', '==', this.profileid)
       );
-      
+
       const draftSnapshot = await getDocs(draftQuery);
       console.log(draftSnapshot.docs.length);
-      
+
       if (!draftSnapshot.empty) {
         draftSnapshot.docs.forEach(draftDoc => {
           draftforms.push(draftDoc);
@@ -856,31 +858,31 @@ export class FormtemplateComponent {
       for (let k = 0; k < element['array'].length; k++) {
         const arrayelement = element['array'][k];
         arrayelement['formarraycontrol'] = `arraycontrol${k}`;
-        
+
         const formArrayControl = this.deliveryForm.get(element['formcontrol']) as FormArray;
         const arrayGroup = formArrayControl.at(j);
 
         if (!['date', 'label', 'array'].includes(arrayelement['type'])) {
           // Regular array form controls
-          const value = element['value'][j][arrayelement['fieldname']] 
-            ?? element['value'][j][arrayelement['formarraycontrol']] 
+          const value = element['value'][j][arrayelement['fieldname']]
+            ?? element['value'][j][arrayelement['formarraycontrol']]
             ?? null;
           arrayGroup?.get(arrayelement['formarraycontrol'])?.patchValue(value);
-          
+
         } else if (arrayelement['type'] === 'date') {
           // Date array form controls
           let dateValue = null;
-          
+
           if (element['value'][j][arrayelement['fieldname']] !== undefined) {
             dateValue = element['value'][j][arrayelement['fieldname']]?.toDate();
           } else if (element['value'][j][arrayelement['formarraycontrol']]) {
             dateValue = element['value'][j][arrayelement['formarraycontrol']]?.toDate();
           }
-          
+
           arrayGroup?.get(arrayelement['formarraycontrol'])?.patchValue(dateValue);
         }
       }
     }
   }
-  
+
 }
