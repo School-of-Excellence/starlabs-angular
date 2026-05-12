586a96f4b831201d70a4fe404e7f83c087179487
Nandakumar23 <nandakumar.m@soexcellence.com>
2026-05-12 12:16:00 +0530

    workshop config changes

--- src/app/New-Workshop/workshop-configuration/workshop-configuration.component.ts
+++ src/app/New-Workshop/workshop-configuration/workshop-configuration.component.ts
@@ -25,6 +25,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 import { A, COMMA, ENTER } from '@angular/cdk/keycodes';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
@@ -153,6 +154,7 @@ interface CurriculumItem {
     MatSelectModule,
     MatTabsModule,
     MatTooltipModule,
+    MatMenuModule,
     DragDropModule,
     NgxEditorModule,
     MatSlideToggleModule,
@@ -943,6 +945,8 @@ dropChallengeOuter(event: CdkDragDrop<AbstractControl[]>) {
       facilitatorprofiles:[[],],
       selectedgroup: [''],
       enrollwattimessage: [''],
+      loginlogchannel:['workshop-logs'],
+      workshopactivitychannel:['workshop-logs'],
       selectedjourneys: [[],],
       selectedtiers: [[],],
       categoriesforthisworkshop:[[],],
@@ -1574,12 +1578,16 @@ dropChallengeOuter(event: CdkDragDrop<AbstractControl[]>) {
     return (curriculumGroup.get('challenges'));
   }
 
-  addSubChallenge(curriculumGroup) {
+  addSubChallenge(curriculumGroup, afterIndex?: number) {
     const challengeIndex = this.challengesArray.controls.indexOf(curriculumGroup);
-    const activityIndex = this.getChallengeArray(curriculumGroup).length;
+    const challengeArray = this.getChallengeArray(curriculumGroup);
+    const insertIndex = (afterIndex === undefined || afterIndex === null)
+      ? challengeArray.length
+      : afterIndex + 1;
+    const newChallengeId = this.generateId();
     const challengeGroup = this.fb.group({
-      challengeid: [this.generateId()], 
-      zoomattend: [[]], 
+      challengeid: [newChallengeId],
+      zoomattend: [[]],
       name: ['',],
       description: ['',],
       type:['',],
@@ -1615,8 +1623,19 @@ dropChallengeOuter(event: CdkDragDrop<AbstractControl[]>) {
       
     });
 
-    this.getChallengeArray(curriculumGroup).push(challengeGroup);
-    this.initializeNoteEditor(challengeIndex, activityIndex);
+    if (insertIndex >= challengeArray.length) {
+      challengeArray.push(challengeGroup);
+    } else {
+      challengeArray.insert(insertIndex, challengeGroup);
+    }
+    this.initializeNoteEditor(challengeIndex, insertIndex);
+    this.rebuildActivityIds();
+    setTimeout(() => {
+      const el = document.querySelector(`[data-activity-id="${newChallengeId}"]`) as HTMLElement | null;
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    }, 100);
   }
   // removeSubChallenge(curriculumGroup, index) {
   //   const confirmDelete = confirm("Are you sure you want to delete this sub-challenge?");
@@ -2047,6 +2066,8 @@ private rebuildActivityIds(): void {
         facilitatorprofiles: data['facilitatorprofiles'] || [],
         selectedgroup: data['selectedgroup'] || null,
         enrollwattimessage: data['enrollwattimessage'] || null,
+        loginlogchannel: data['loginlogchannel'] || 'workshop-logs',
+        workshopactivitychannel: data['workshopactivitychannel'] || 'workshop-logs',
         mailTemplate: data['mailTemplate'] || null,
         selectedjourneys: data['selectedjourneys'] || [],
         selectedtiers: data['selectedtiers'] || [],
@@ -2214,7 +2235,9 @@ private rebuildActivityIds(): void {
         facilitator: this.settingsForm.get('facilitator')?.value || false,
         facilitatorprofiles: this.settingsForm.get('facilitatorprofiles')?.value || [],
         selectedgroup: this.settingsForm.get('selectedgroup')?.value || null,
+        loginlogchannel: this.settingsForm.get('loginlogchannel')?.value || null,
         enrollwattimessage: this.settingsForm.get('enrollwattimessage')?.value || null,
+        workshopactivitychannel: this.settingsForm.get('workshopactivitychannel')?.value || null,
         mailTemplate: this.settingsForm.get('mailTemplate')?.value || null,
         testusers: this.settingsForm.get('testusers')?.value || [],
         selectedjourneys: this.settingsForm.get('selectedjourneys')?.value || [],
