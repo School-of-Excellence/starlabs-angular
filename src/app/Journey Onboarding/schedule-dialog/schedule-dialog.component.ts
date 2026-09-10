2f2e1016b6430468ce8b986e65e35435633b0e95
Charan Reddy <macbook@Charan-Reddy-2.local>
2026-09-10 16:08:52 +0530

    product onboarding

--- src/app/Journey Onboarding/schedule-dialog/schedule-dialog.component.ts
+++ src/app/Journey Onboarding/schedule-dialog/schedule-dialog.component.ts
@@ -16,7 +16,6 @@ import { MatCalendar } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { platform } from 'os';
 
 @Component({
   selector: 'app-schedule-dialog',
@@ -198,8 +197,12 @@ export class ScheduleDialogComponent {
     // });
 
     this.loading = true;
-    let dataQuery = this.participantjourneyproduct.calltype == 'coach' ? 'journeycoach' : 'onboardingcall';
-    await getDocs(query(collection(this.firestore, "appointmenttype"), where(dataQuery, '==', true))).then(res => {
+    const isProductOnboarding = this.participantjourneyproduct.calltype == 'productonboarding';
+    const dataQuery = this.participantjourneyproduct.calltype == 'coach' ? 'journeycoach' : 'onboardingcall';
+
+    const appointmentTypeQuery = isProductOnboarding? query(collection(this.firestore, "appointmenttype"), where('appointmenttype', '==', 'DFU Onboarding')): query(collection(this.firestore, "appointmenttype"), where(dataQuery, '==', true));
+
+    await getDocs(appointmentTypeQuery).then(res => {
       for (let i = 0; i < res.docs.length; i++) {
         const element = res.docs[i].data();
         this.appointmenttypes.push({
@@ -621,6 +624,9 @@ export class ScheduleDialogComponent {
 
         if (this.participantjourneyproduct.calltype == 'onboarding') {
           map['onboarding'] = true;
+        } else if (this.participantjourneyproduct.calltype == 'productonboarding') {
+          map['productonboarding'] = true;
+          map['participantproductid'] = this.participantjourneyproduct['participantsproductid'];
         }
 
         try {
@@ -635,6 +641,14 @@ export class ScheduleDialogComponent {
                 onboardedby: hostRef,
                 appointmentid: newDocRef.id, 
                 orientationstatus: "scheduled"
+              }
+            );
+          } else if (this.participantjourneyproduct.calltype == 'productonboarding') {
+            await updateDoc(doc(this.firestore, "participantsproduct", this.participantjourneyproduct['docid']),
+              {
+                productonboardingscheduled: new Date(selectedSlot.start),
+                productonboardedby: hostRef,
+                productonboarding: true,
               }
             );
           }
